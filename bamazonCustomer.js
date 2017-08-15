7f7227db82033db2e1d4f4d235ed58ed88ca5dba
AgesilausDrako <agesilaus@yahoo.com>
2017-08-15 11:46:57 -0400

    console.table working

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,7 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+var Table = require("cli-table");
+require("console.table");
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -9,32 +11,59 @@ var connection = mysql.createConnection({
     database: "bamazon"
   });
 
-function appInit() {
+  connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+    start();
+  });
+
+function createTable() {
   connection.query("SELECT * FROM products", function(err, results) {
-      if (err) throw err;
-      inquirer
-        .prompt([
-          {
-            name: "choice",
-            type: "rawlist",
-            choices: function() {
-              var choiceArray = [];
-              for (var i = 0; i < results.length; i++) {
-                choiceArray.push(results[i].item_id);
-              }
-              return choiceArray;
-            },
-            message: "What product would you like to buy?"
-          },
-          {
-            name: "buy",
-            type: "input",
-            message: "How many would you like to purchase?"
-          }
-        ]).then(function(answer) {
-          console.log(answer);
-        });
+    if (err) throw err;
+    
+    console.table(results);
+    
   });
 }
 
-appInit();
+function start() {
+  createTable();
+  connection.query("SELECT * FROM products", function(err, results) {
+    if (err) throw err;
+    inquirer
+      .prompt([
+        {
+          name: "choice",
+          type: "list",
+          choices: function() {
+            var choiceArray = [];
+            for (var i = 0; i < results.length; i++) {
+              choiceArray.push(results[i].item_id + "  " + results[i].product_name);
+            }
+            return choiceArray;
+          },
+          message: "What product would you like to buy?\n"
+        },
+        {
+          name: "purchase_quantity",
+          type: "input",
+          message: "How many would you like to buy?\n"
+        }
+      ]).then(function(answer) {
+        // get the information of the chosen item
+        var chosenItem;
+        for (var i = 0; i < results.length; i++) {
+          if (results[i].item_id === answer.choice) {
+            chosenItem = results[i];
+          }
+        }
+        console.log(answer.purchase_quantity);
+        if (answer.purchase_quantity > chosenItem.stock_quantity) {
+          console.log("Insufficient quantity");
+          return false;
+        } else {
+          return true;
+        }
+      });
+  });
+}
--- bamazonSchema.sql
+++ bamazonSchema.sql
@@ -13,5 +13,22 @@ CREATE TABLE products(
 );
 
 INSERT INTO products (product_name, department_name, price, stock_quantity) 
+VALUES ("Xbox", "Electronics", 279, 200);
+INSERT INTO products (product_name, department_name, price, stock_quantity) 
+VALUES ("Flatscreen HD TV", "Electronics", 599, 100);
+INSERT INTO products (product_name, department_name, price, stock_quantity) 
+VALUES ("Oak Bookcase", "Furniture", 80, 50);
+INSERT INTO products (product_name, department_name, price, stock_quantity) 
+VALUES ("Couch", "Furniture", 999, 45);
+INSERT INTO products (product_name, department_name, price, stock_quantity) 
+VALUES ("Car Battery", "Automotive", 49, 200);
+INSERT INTO products (product_name, department_name, price, stock_quantity) 
 VALUES ("Car Tires", "Automotive", 65, 500);
-
+INSERT INTO products (product_name, department_name, price, stock_quantity) 
+VALUES ("Motor Oil", "Automotive", 25, 1500);
+INSERT INTO products (product_name, department_name, price, stock_quantity) 
+VALUES ("Refrigerator", "Appliances", 1600, 200);
+INSERT INTO products (product_name, department_name, price, stock_quantity) 
+VALUES ("Washer-Dryer Combo", "Appliances", 1398, 75);
+INSERT INTO products (product_name, department_name, price, stock_quantity) 
+VALUES ("Exterior Paint", "Paint", 45, 800);
